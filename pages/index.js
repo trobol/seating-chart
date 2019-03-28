5082e0adeb3895a2f0e15a17e7c0a067088662fc
kevineaton603 <kevin.eaton@mymail.champlain.edu>
2019-03-28 12:44:09 -0400

    WhenIWork integration complete

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout';
 const Index = () => (
   <Layout>
     <p>Hello Next.js</p>
-    <form action="/api/test-api" method="post">
+    <form action="/api/wheniwork" method="post">
       <input type="text" name="name" placeholder="name" />
       <input type="submit" value="Submit" />
     </form>
--- server/routes/api/wheniwork.js
+++ server/routes/api/wheniwork.js
@@ -3,9 +3,22 @@ let WIW = require('wheniwork').WIW;
 const WIW_API_KEY = process.env.WIW_API_KEY
 const WIW_USERNAME = process.env.WIW_USERNAME
 const WIW_PASSWORD = process.env.WIW_PASSWORD
+const WIW_ACCOUNT_ID = process.env.WIW_ACCOUNT_ID
 module.exports = (app)=>{
-    app.get("api/wheniwork.js", (req, res)=>{
-        res.send({ response: 'successs' });
+    app.post("/api/wheniwork", (req, res)=>{
+        let endpoint = req.body.endpoint;
+        let wiw = new WIW(WIW_API_KEY, WIW_USERNAME, WIW_PASSWORD);
+        let parameters = new JSON();
+        wiw.config.accountId = WIW_ACCOUNT_ID;
+        switch (endpoint){
+        case 'times/clockin':
+        case 'times/clockout':
+            parameters = {"id": req.body.id};
+            break;
+        }
+        wiw.post(endpoint, parameters).then((result)=>{
+            console.log(res)
+        })
         console.log({WIW_API_KEY, WIW_USERNAME, WIW_PASSWORD})
     })
 }
