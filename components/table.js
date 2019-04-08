cc7c7907d8b32fbc675fbebad21ac8f61e44aba0
jmichaels <jmichaels@localhost>
2019-04-08 15:30:27 -0400

    made table class json driven

--- components/table.js
+++ components/table.js
@@ -4,18 +4,9 @@ import MaterialTable from 'material-table';
 class Table extends React.Component {
   constructor(props) {
     super(props);
-    const jsonString = '{"users": [{"name": "kevin","id": 0,"seat": 5},{"name": "adam","id": 1,"seat": 6},{"name": "jackson","id": 2,"seat": 7}]}';
-    const obj = JSON.parse(jsonString);
-    const tmp = [
-      {
-        name: 'Kevin', surname: 'Eats', seat: 8,
-      }, {
-        name: 'Adam', surname: 'Pasta', seat: 11,
-      }, {
-        name: 'Jackson', surname: 'Mic', seat: 7,
-      },
-    ];
-    this.state = { users: props.users, data: tmp, debug: obj };
+    const tableData = JSON.parse(props.contents);
+    const tableHeader = JSON.parse(props.header);
+    this.state = { head: tableHeader, data: tableData };
   }
 
   render() {
@@ -23,14 +14,9 @@ class Table extends React.Component {
       <div>
         <div>
           <span>Test: </span>
-          {this.state.debug.name}
         </div>
         <MaterialTable
-          columns={[
-            { title: 'Firstname', field: 'name' },
-            { title: 'LastName', field: 'surname' },
-            { title: 'Seat', field: 'seat', type: 'numeric' },
-          ]}
+          columns={this.state.head}
           data={this.state.data}
           title="Admin Site"
         />
