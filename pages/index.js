aea8dacc22dda4a36ecdc1a32cc3ef9b827c1087
kevineaton603 <kevin.eaton@mymail.champlain.edu>
2019-04-22 16:16:52 -0400

    working on reservations

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 import Layout from '../components/Layout';
-import Map from '../components/map';
-import SeatCircle from '../components/map/seatCircle';
+// import Map from '../components/map';
+// import SeatCircle from '../components/map/seatCircle';
 
 const getReservations = () => {
-  console.log('clicked');
-  axios.get('/api/seat/reserve').then((res) => {
+  axios.get('/api/seat/reserve', { params: { seat: 1 } }).then((res) => {
     console.log(res);
   });
 };
@@ -24,6 +23,14 @@ const Index = () => (
     </form>
     <form action="/user/manage" method="get">
       <input type="submit" value="Submit" />
+    </form>
+    <form action="api/seat/reserve" method="post">
+      <div>Reserve your seat</div>
+      <input type="datetime-local" name="expires" />
+      <input type="number" name="seat" />
+      <input type="time" name="start" />
+      <input type="time" name="end" />
+      <input type="text" name="reasons" />
     </form>
     <button type="button" onClick={() => getReservations()}>Click Me!</button>
   </Layout>
--- server/routes/api/seat/reserve.js
+++ server/routes/api/seat/reserve.js
@@ -2,12 +2,32 @@ const mysql = require('mysql');
 
 module.exports = (app, isLoggedIn) => {
   app.post('/api/seat/reserve/', isLoggedIn, (req, res) => {
-    res.send({ response: 'success' });
+    const { idusers } = req.user;
+    const {
+      seat, weekday, start, end, expires, reason,
+    } = req.body;
+    if (![seat, weekday, start, end, expires, reason, idusers].filter(e => e === null).length) {
+      // If reservation is available
+      const getReservationSql = mysql.format(
+        'SELECT * FROM `reservations` WHERE `reservations`.`s_id` = ? AND `reservations`.`weekday` = ? AND `reservations`.`expires` > NOW();',
+        [seat, weekday],
+      );
+      app.pool.query(getReservationSql, (err, result) => {
+        const filtered = result.filter(r => start <= r.end && end >= r.start);
+        res.send({ response: filtered });
+        // Then Insert into reservation
+
+        // Else return error
+      });
+    } else {
+      res.send({ response: 'success' });
+    }
   });
   app.get('/api/seat/reserve', (req, res) => {
-    res.send({ response: 'success' });
+    const { seat } = req.query;
+    const getReservationSql = mysql.format('SELECT * FROM `reservations` WHERE `reservations`.`s_id` = ?', [seat]);
+    app.pool.query(getReservationSql, (err, result) => {
+      res.send({ response: result });
+    });
   });
-  /* app.post('/api/seat/reserve/', isLoggedIn, (req, res) => {
-    res.send({ response: 'post success' });
-  }); */
 };
