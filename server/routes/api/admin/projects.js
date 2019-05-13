4d30bb03d0440c289d9ad8ab2ffcf0e8437aeb61
kevineaton603 <kevin.eaton@mymail.champlain.edu>
2019-05-13 15:01:15 -0400

    projects api

--- server/routes/api/admin/projects.js
+++ server/routes/api/admin/projects.js
@@ -1,17 +1,37 @@
 
+const mysql = require('mysql');
+
 module.exports = (app, isLoggedIn, isAdmin) => {
   // Gets All Project Info
-  app.get('/api/admin/projects/', isAdmin, (req, res) => {
-    const sql = '';
-    res.send({ response: 'success' });
+  app.get('/api/admin/projects/', isLoggedIn, isAdmin, (req, res) => {
+    const sql = 'SELECT `project`, FROM `projects`';
+    app.pool.query(sql, (error, results, fields) => {
+      if (error) throw error;
+      res.send(results, fields);
+    });
+    res.send({ response: 'failure' });
   });
   // Gets Specific Project Info
   app.get('/api/admin/projects/:projectId', isLoggedIn, isAdmin, (req, res) => {
-    res.send({ response: 'success' });
+    const uSql = 'SELECT u.`name` FROM `projects` as p INNER JOIN `user_project` as up ON up.`p_id` = p.`idprojects` INNER JOIN `users` as u ON up.`u_id` = u.`idusers` WHERE p.`idprojects`=?';
+    const { pid } = req.body;
+    const sql = mysql.format(uSql, [pid]);
+    app.pool.query(sql, (error, results, fields) => {
+      if (error) throw error;
+      res.send({ response: { results, fields } });
+    });
+    res.send({ response: 'failure' });
   });
   // Posts new project
   app.post('/api/admin/projects/', isLoggedIn, isAdmin, (req, res) => {
-    res.send({ response: 'success' });
+    const uSql = 'INSERT INTO `projects`(`project`) VALUES (?)';
+    const { project } = req.body;
+    const sql = mysql.format(uSql, [project]);
+    app.pool.query(sql, (error, results, fields) => {
+      if (error) throw error;
+      res.send({ response: { results, fields } });
+    });
+    res.send({ response: 'failure' });
   });
   // Deletes a project
   app.post('api/admin/projects/:projectId', isLoggedIn, isAdmin, (req, res) => {
