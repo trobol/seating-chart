11ffb32b5c9f0e1204ae69f90a2058344f28f9dd
Kevin Eaton <keaton@Admins-iMac.local>
2019-07-29 11:13:10 -0300

    small change

--- server/routes/api/map/seats.js
+++ server/routes/api/map/seats.js
@@ -2,14 +2,17 @@ module.exports = (app) => {
   app.get('/api/map/seats/', (req, res) => {
     const sql = 'SELECT s.`idseats` as sid, s.`computer_name` as computerName, s.`u_id` as uid, u.`name`, u.`image` FROM `seats` as s INNER JOIN `users` as u on s.`u_id` = u.`idusers` WHERE s.`u_id`IS NOT NULL';
     app.pool.query(sql, (err, results) => {
-      // eslint-disable-next-line prefer-const
-      let seats = results;
-      if (seats !== null || seats !== undefined) {
-        for (let i = 0; i < seats.length; i += 1) {
-          seats[i].image = `static/users/${seats[i].image}.jpg`;
+      if (err) res.send({ error: err });
+      else {
+        // eslint-disable-next-line prefer-const
+        let seats = results;
+        if (seats !== null || seats !== undefined) {
+          for (let i = 0; i < seats.length; i += 1) {
+            seats[i].image = `static/users/${seats[i].image}.jpg`;
+          }
         }
+        res.send({ response: { err, seats } });
       }
-      res.send({ response: { err, seats } });
     });
   });
 };
