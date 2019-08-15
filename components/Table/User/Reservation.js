d3ca6fccf6f6c679e5456dc2f4540f6c14c506ad
keaton <kevin.eaton@mymail.champlain.edu>
2019-08-15 12:39:52 -0400

    fixing reservations form

--- components/Table/User/Reservation.js
+++ components/Table/User/Reservation.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
   Table, TableCell, Button, Message,
 } from 'semantic-ui-react';
@@ -10,35 +10,52 @@ import useInterval from '../../Util';
 import { BaseModal } from '../../Modals';
 
 const UserReservationTable = () => {
-  const [data, setData] = useState(null);
+  const [user, setUser] = useState(null);
+  const [userReservations, setUserReservations] = useState(null);
+  const [allReservations, setAllReservations] = useState(null);
   const [selectedReservation, setSelectedReservation] = useState(null);
   const [open, setOpen] = useState(false);
   const [activeModal, setActiveModal] = useState('');
   const deleteAction = (row) => {
     Promise.resolve(axios.post(`/api/users/reservations/${row.id}`));
   };
+  useEffect(() => {
+    Promise.resolve(axios.get('/api/users/get-user/'))
+      .then((res) => {
+        console.log(res.data.user);
+        if (res.data.user) {
+          setUser(res.data.user);
+        }
+      });
+  }, []);
+  useEffect(() => {
+    if (user !== null) {
+      setUserReservations(allReservations.filter(({ uid }) => uid === user.idusers));
+    }
+  }, [allReservations, user]);
+
   useInterval(() => {
     Promise.resolve(axios.get('/api/users/reservations/')).then((res) => {
       console.log({ res });
       const { result } = res.data;
-      if (!_.isEqual(result, data)) {
-        setData(result);
+      if (!_.isEqual(result, allReservations)) {
+        setAllReservations(result);
       }
     });
   }, 1000);
   return (
     <>
-      {data !== null && data !== undefined && !_.isEmpty(data) ? (
+      {userReservations !== null && userReservations !== undefined && !_.isEmpty(userReservations) ? (
         <Table celled compact selectable sortable>
           <Table.Header>
             <Table.Row>
-              {Object.keys(data[0]).map((key => <Table.HeaderCell key={key}>{key}</Table.HeaderCell>))}
+              {Object.keys(userReservations[0]).map((key => <Table.HeaderCell key={key}>{key}</Table.HeaderCell>))}
               <Table.HeaderCell>Delete</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
           <Table.Body>
-            {data.map(row => (
-              <Table.Row onMouseEnter={() => setSelectedReservation(data[row.id - 1])}>
+            {userReservations.map(row => (
+              <Table.Row onMouseEnter={() => setSelectedReservation(userReservations[row.id - 1])}>
                 {
             Object.keys(row).map((key) => {
               if (key === 'expires') {
@@ -55,7 +72,7 @@ const UserReservationTable = () => {
           </Table.Body>
           <Table.Footer fullWidth>
             <Table.Row>
-              <Table.HeaderCell colSpan={Object.keys(data[0]).length + 1}>
+              <Table.HeaderCell colSpan={Object.keys(userReservations[0]).length + 1}>
                 <Button icon="time" content="Add Reservation" onClick={() => setActiveModal('add-reservations')} />
               </Table.HeaderCell>
             </Table.Row>
